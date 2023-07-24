b143b3dc6355046f71aa7979348b6d3847f8ace5
JestonieLacaden <lacadenjestonie28@gmail.com>
2023-07-24 19:15:48 +0800

    Create a new Login Form

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,7 +19,7 @@ function Navbar() {
                             </div>
                         </li>
                         <li>
-                            <div class="user-icon"><FontAwesomeIcon icon={faUser} /></div>
+                            <Link to="/LoginPage"><div class="user-icon"><FontAwesomeIcon icon={faUser} /></div></Link>
                         </li>
                     </ul>
                 </nav>
